1d77b049ac56361ec6d7863a7fd0d6b40a84fa2f
Zadigo <pendenquejohn@gmail.com>
2022-06-26 22:20:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition name="site-transition" mode="in-out">
+    <component :is="currentSite" />
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
